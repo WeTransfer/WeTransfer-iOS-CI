4135f7ee20fb091a85067230e3b3617cd174e988
Antoine van der Lee <antoine@wetransfer.com>
2018-03-29 16:52:41 +0200

    Fix robocop warnings

--- Danger/ext/git_swift_linter.rb
+++ Danger/ext/git_swift_linter.rb
@@ -1,6 +1,5 @@
 # GIT Warnings and errors report of the lint.
 class GitSwiftLinterReport
-
   attr_accessor :warnings, :errors
 
   def initialize(warnings, errors)
@@ -20,26 +19,26 @@ class GitSwiftLinter
   def lint
     warnings = []
 
-    warnings.push("Big PR") if danger_file.git.lines_of_code > 500
+    warnings.push('Big PR') if danger_file.git.lines_of_code > 500
 
-    return GitSwiftLinterReport.new(warnings, [])
+    GitSwiftLinterReport.new(warnings, [])
   end
 
   # Warn if we submitted a big PR.
   def lines_of_code
-    danger_file.warn("Big PR") if danger_file.git.lines_of_code > 500
+    danger_file.warn('Big PR') if danger_file.git.lines_of_code > 500
   end
 
   # Mainly to encourage writing up some reasoning about the PR, rather than just leaving a title.
   def pr_description
-    danger_file.warn("Please provide a summary in the Pull Request description") if danger_file.github.pr_body.length < 3 && danger_file.git.lines_of_code > 10
+    danger_file.warn('Please provide a summary in the Pull Request description') if danger_file.github.pr_body.length < 3 && danger_file.git.lines_of_code > 10
   end
 
   # Make it more obvious that a PR is a work in progress and shouldn't be merged yet.
   def work_in_progress
-    has_wip_label = danger_file.github.pr_labels.any? { |label| label.include? "WIP" }
-    has_wip_title = danger_file.github.pr_title.include? "[WIP]"
+    has_wip_label = danger_file.github.pr_labels.any? { |label| label.include? 'WIP' }
+    has_wip_title = danger_file.github.pr_title.include? '[WIP]'
 
-    danger_file.warn("PR is classed as Work in Progress") if has_wip_label || has_wip_title
+    danger_file.warn('PR is classed as Work in Progress') if has_wip_label || has_wip_title
   end
 end
--- Danger/spec/git_swift_linter_spec.rb
+++ Danger/spec/git_swift_linter_spec.rb
@@ -1,42 +1,40 @@
 require File.expand_path('../spec_helper', __FILE__)
 require File.expand_path('../../ext/git_swift_linter', __FILE__)
 
-module Danger
-  describe GitSwiftLinter do
-    before do
-      @gitswiftlinter = GitSwiftLinter.new(testing_dangerfile)
+describe GitSwiftLinter do
+  before do
+    @gitswiftlinter = GitSwiftLinter.new(testing_dangerfile)
+  end
+
+  describe :lint do
+    it 'Warns for a big PR' do
+      allow(@gitswiftlinter.danger_file.git).to receive(:lines_of_code).and_return(600)
+      expect(@gitswiftlinter.danger_file).to receive(:warn).with('Big PR')
+      @gitswiftlinter.lines_of_code
     end
 
-    describe :lint do
-      it "Warns for a big PR" do
-        allow(@gitswiftlinter.danger_file.git).to receive(:lines_of_code).and_return(600)
-        expect(@gitswiftlinter.danger_file).to receive(:warn).with("Big PR")
-        @gitswiftlinter.lines_of_code
-      end
+    it 'Warns for small descriptions' do
+      allow(@gitswiftlinter.danger_file.git).to receive(:lines_of_code).and_return(11)
+      allow(@gitswiftlinter.danger_file.github).to receive(:pr_body).and_return('ab')
+      expect(@gitswiftlinter.danger_file).to receive(:warn).with('Please provide a summary in the Pull Request description')
 
-      it "Warns for small descriptions" do
-        allow(@gitswiftlinter.danger_file.git).to receive(:lines_of_code).and_return(11)
-        allow(@gitswiftlinter.danger_file.github).to receive(:pr_body).and_return("ab")
-        expect(@gitswiftlinter.danger_file).to receive(:warn).with("Please provide a summary in the Pull Request description")
-        
-        @gitswiftlinter.pr_description
-      end
+      @gitswiftlinter.pr_description
+    end
 
-      it "Warns for WIP labels" do
-        allow(@gitswiftlinter.danger_file.github).to receive(:pr_labels).and_return(["WIP"])
-        allow(@gitswiftlinter.danger_file.github).to receive(:pr_title).and_return(["Pull Request Title"])
-        expect(@gitswiftlinter.danger_file).to receive(:warn).with("PR is classed as Work in Progress")
-        
-        @gitswiftlinter.work_in_progress
-      end
+    it 'Warns for WIP labels' do
+      allow(@gitswiftlinter.danger_file.github).to receive(:pr_labels).and_return(['WIP'])
+      allow(@gitswiftlinter.danger_file.github).to receive(:pr_title).and_return(['Pull Request Title'])
+      expect(@gitswiftlinter.danger_file).to receive(:warn).with('PR is classed as Work in Progress')
 
-      it "Warns for [WIP] in PR titles" do
-        allow(@gitswiftlinter.danger_file.github).to receive(:pr_labels).and_return([])
-        allow(@gitswiftlinter.danger_file.github).to receive(:pr_title).and_return("Pull Request which is [WIP]")
-        expect(@gitswiftlinter.danger_file).to receive(:warn).with("PR is classed as Work in Progress")
-        
-        @gitswiftlinter.work_in_progress
-      end
+      @gitswiftlinter.work_in_progress
+    end
+
+    it 'Warns for [WIP] in PR titles' do
+      allow(@gitswiftlinter.danger_file.github).to receive(:pr_labels).and_return([])
+      allow(@gitswiftlinter.danger_file.github).to receive(:pr_title).and_return('Pull Request which is [WIP]')
+      expect(@gitswiftlinter.danger_file).to receive(:warn).with('PR is classed as Work in Progress')
+
+      @gitswiftlinter.work_in_progress
     end
   end
 end

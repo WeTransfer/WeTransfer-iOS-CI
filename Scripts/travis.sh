2aab5f1cf9327e97a2de259867d99b39af7801c0
Pim Coumans <pim@pixelrock.nl>
2018-09-24 14:07:50 +0200

    Added support for defining specific scheme

--- Scripts/travis.sh
+++ Scripts/travis.sh
@@ -3,4 +3,4 @@ git submodule sync && git submodule update --init --recursive
 brew update
 brew outdated swiftlint || brew upgrade swiftlint
 bundle install
-bundle exec fastlane test project_name:$1
+bundle exec fastlane test project_name:$1 scheme:$2

d5f3c5aefccf17389e5a65389b913f19ac4fc996
Antoine van der Lee <antoine@wetransfer.com>
2020-01-13 10:14:09 +0100

    Update DangerFakeSource to be more describing

--- Danger-Swift/DangerFakeSource.swift
+++ Danger-Swift/DangerFakeSource.swift
@@ -1,1 +1,3 @@
-/// Is used for the Package.swift to have a fake Swift source file.
+/// Is used for the Package.swift in implementing projects to have a fake Swift source file.
+/// E.g:
+/// .target(name: "DangerDependencies", dependencies: ["Danger", "WeTransferPRLinter"], path: "Submodules/WeTransfer-iOS-CI/Danger-Swift", sources: ["DangerFakeSource.swift"]),

afaebb4e7c145f25b1139b0008993b2e069cd398
Antoine van der Lee <antoine@wetransfer.com>
2020-01-15 11:16:55 +0100

    Prevent failing if there's no test files to lint

--- SwiftLint/swiftlint.sh
+++ SwiftLint/swiftlint.sh
@@ -2,7 +2,7 @@ if [ -z "$CI" ]; then
     if which swiftlint >/dev/null; then
     	BASEDIR=$(dirname "$0")
         swiftlint --config "$BASEDIR/.swiftlint-source.yml"
-        swiftlint --config "$BASEDIR/.swiftlint-tests.yml"
+        swiftlint --config "$BASEDIR/.swiftlint-tests.yml" || true # Don't fail if there's no tests to lint
     else
     	echo "error: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
     fi

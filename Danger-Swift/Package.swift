539d146dddc52c9ca84b6f5148863a1eff0d9d85
Antoine van der Lee <antoine@wetransfer.com>
2020-01-23 11:22:38 +0100

    Use the branch for now

--- Danger-Swift/Package.swift
+++ Danger-Swift/Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/danger/swift", from: "3.0.0"),
         .package(url: "https://github.com/f-meloni/danger-swift-coverage", from: "1.1.0"),
-        .package(url: "https://github.com/f-meloni/danger-swift-xcodesummary", from: "1.2.0"),
+        //.package(url: "https://github.com/f-meloni/danger-swift-xcodesummary", from: "1.2.0"),
+        .package(url: "https://github.com/AvdLee/danger-swift-xcodesummary.git", .branch("feature/duplicate-filtering")),
         .package(url: "https://github.com/JohnSundell/Files", from: "4.1.1")
     ],
     targets: [

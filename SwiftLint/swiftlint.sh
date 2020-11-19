1a7c2cbcd59171e9c9ed001676d32b5353809147
Antoine van der Lee <ajvanderlee@gmail.com>
2020-11-19 02:30:12 -0800

    Only lint files that are changed (#67)
    * Only lint files that are changed
    
    * Ignore vendor changes to make sure we don't lint files twice
    
    * Add support for files with spaces. Only lint relative changes to the project to support inner frameworks using the same script.
    
    * Update SwiftLint/swiftlint.sh

--- SwiftLint/swiftlint.sh
+++ SwiftLint/swiftlint.sh
@@ -1,10 +1,33 @@
+#!/bin/bash
 if [ -z "$CI" ]; then
 	set -e
 	
     if which swiftlint >/dev/null; then
     	BASEDIR=$(dirname "$0")
-        swiftlint --config "$BASEDIR/.swiftlint-source.yml"
-        swiftlint --config "$BASEDIR/.swiftlint-tests.yml" || true # Don't fail if there's no tests to lint
+    	count=0
+
+    	# Unstaged files
+		while read filename; do 
+			export SCRIPT_INPUT_FILE_$count="${filename}"
+			count=$((count + 1))
+		done < <(git diff --relative --name-only $SRCROOT | grep ".swift$")
+
+		# Staged files
+		while read filename; do 
+			export SCRIPT_INPUT_FILE_$count="${filename}"
+			count=$((count + 1))
+		done < <(git diff --relative --diff-filter=d --cached --name-only $SRCROOT | grep ".swift$")
+
+		export SCRIPT_INPUT_FILE_COUNT=$count
+
+		if (( $count > 0 )); then
+	        echo "Found lintable files! Linting..."
+	        swiftlint lint --use-script-input-files --config "$BASEDIR/.swiftlint-source.yml" --force-exclude;
+		    swiftlint lint --use-script-input-files --config "$BASEDIR/.swiftlint-tests.yml" --force-exclude || true; # Don't fail if there's no tests to lint
+	    else
+	        echo "No files to lint, the number of files found is $count"
+	        exit 0
+	    fi
     else
     	echo "error: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
     fi

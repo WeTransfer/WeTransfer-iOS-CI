58b57a71f48ffc6ce2e0fdad8f68b4d286eaf9ed
Antoine van der Lee <ajvanderlee@gmail.com>
2021-10-27 12:00:21 +0200

    Fix Danger

--- WeTransferPRLinter/Package.swift
+++ WeTransferPRLinter/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,9 +16,9 @@ let package = Package(
     ],
     dependencies: [
         // Local package is required for development since Danger does not support Swift 5.5 correctly yet with Danger-Plugin testing.
-        // .package(name: "danger-swift", path: "../../../../../Forks/swift"),
-//        .package(name: "danger-swift", url: "https://github.com/danger/swift.git", from: "3.12.1"),
-        .package(name: "danger-swift", url: "https://github.com/AvdLee/swift.git", .branch("master")),
+//         .package(name: "danger-swift", path: "../../../../../Forks/swift"),
+        .package(name: "danger-swift", url: "https://github.com/danger/swift.git", from: "3.12.1"),
+//        .package(name: "danger-swift", url: "https://github.com/AvdLee/swift.git", .branch("master")),
         .package(name: "Files", url: "https://github.com/JohnSundell/Files", from: "4.1.1"),
         .package(name: "XCResultKit", url: "https://github.com/davidahouse/XCResultKit.git", from: "0.9.2")
     ],

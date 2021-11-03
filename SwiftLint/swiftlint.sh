7b78d925c383e12e63935e31774615c5724becbf
Kevin Renskers <kevin@loopwerk.io>
2021-11-03 12:24:35 +0100

    Use mint to run SwiftLint

--- SwiftLint/swiftlint.sh
+++ SwiftLint/swiftlint.sh
@@ -1,47 +1,43 @@
 #!/bin/bash
 if [ -z "$CI" ]; then
-	set -e
-	
-    if which swiftlint >/dev/null; then
-    	BASEDIR=$(dirname "$0") # Sets the folder to WeTransfer-iOS-CI/SwiftLint/
-    	execution_directory="$(pwd)"
-    	count=0
-    	export SRCROOT="$(pwd)"
+  set -e
 
-    	printf "\nExecuting SwiftLint from ${execution_directory}\n"
+  BASEDIR=$(dirname "$0") # Sets the folder to WeTransfer-iOS-CI/SwiftLint/
+  execution_directory="$(pwd)"
+  count=0
+  export SRCROOT="$(pwd)"
 
-    	# Unstaged files
-		while read filename; do 
-			export SCRIPT_INPUT_FILE_$count="${filename}"
-			echo "Found '${filename}'"
-			count=$((count + 1))
-		done < <(git diff --relative --name-only $SRCROOT | grep ".swift$")
+  printf "\nExecuting SwiftLint from ${execution_directory}\n"
 
-		# Staged files
-		while read filename; do 
-			export SCRIPT_INPUT_FILE_$count="${filename}"
-			echo "Found '${filename}'"
-			count=$((count + 1))
-		done < <(git diff --relative --diff-filter=d --cached --name-only $SRCROOT | grep ".swift$")
+  # Unstaged files
+  while read filename; do 
+    export SCRIPT_INPUT_FILE_$count="${filename}"
+    echo "Found '${filename}'"
+    count=$((count + 1))
+  done < <(git diff --relative --name-only $SRCROOT | grep ".swift$")
 
-		# Committed files
-		while read filename; do 
-			export SCRIPT_INPUT_FILE_$count="${filename}"
-			echo "Found '${filename}'"
-			count=$((count + 1))
-		done < <(git diff develop... --relative --diff-filter=d --name-only $SRCROOT | grep ".swift$")
+  # Staged files
+  while read filename; do 
+    export SCRIPT_INPUT_FILE_$count="${filename}"
+    echo "Found '${filename}'"
+    count=$((count + 1))
+  done < <(git diff --relative --diff-filter=d --cached --name-only $SRCROOT | grep ".swift$")
 
-		export SCRIPT_INPUT_FILE_COUNT=$count
+  # Committed files
+  while read filename; do 
+    export SCRIPT_INPUT_FILE_$count="${filename}"
+    echo "Found '${filename}'"
+    count=$((count + 1))
+  done < <(git diff develop... --relative --diff-filter=d --name-only $SRCROOT | grep ".swift$")
 
-		if (( $count > 0 )); then
-	        echo "Found ${count} lintable files! Linting..."
-	        swiftlint lint --use-script-input-files --config "$BASEDIR/.swiftlint-source.yml" --force-exclude || true;
-		    swiftlint lint --use-script-input-files --config "$BASEDIR/.swiftlint-tests.yml" --force-exclude || true; # Don't fail if there's no tests to lint
-	    else
-	        echo "No files to lint, the number of files found is $count"
-	        exit 0
-	    fi
-    else
-    	echo "error: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-    fi
+  export SCRIPT_INPUT_FILE_COUNT=$count
+
+  if (( $count > 0 )); then
+    echo "Found ${count} lintable files! Linting..."
+    xcrun --sdk macosx mint run realm/SwiftLint@0.45.0 lint --use-script-input-files --config "$BASEDIR/.swiftlint-source.yml" --force-exclude || true;
+    xcrun --sdk macosx mint run realm/SwiftLint@0.45.0 lint --use-script-input-files --config "$BASEDIR/.swiftlint-tests.yml" --force-exclude || true; # Don't fail if there's no tests to lint
+  else
+    echo "No files to lint, the number of files found is $count"
+    exit 0
+  fi
 fi

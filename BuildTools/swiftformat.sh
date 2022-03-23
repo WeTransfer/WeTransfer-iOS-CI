bca17034e850a6d183c242c503a7306932752340
Raphael Silva <5387088+peagasilva@users.noreply.github.com>
2022-03-23 14:40:42 +0100

    Install mint packages in case SwiftFormat is not installed

--- BuildTools/swiftformat.sh
+++ BuildTools/swiftformat.sh
@@ -2,6 +2,16 @@
 
 if [ -z "$CI" ]; then
     export PATH="$PATH:/opt/homebrew/bin:/usr/local/bin"
+
     BASEDIR=$(dirname "$0")
-    xcrun --sdk macosx mint run -m "$BASEDIR/Mintfile" SwiftFormat --config "$BASEDIR/.swiftformat" .
+    MINT_FILE_PATH=$BASEDIR/Mintfile
+    SWIFT_FORMAT=SwiftFormat
+
+    SWIFT_FORMAT_VERSION=$(grep -F $SWIFT_FORMAT $MINT_FILE_PATH)
+
+    if ! xcrun --sdk macosx mint which -s $SWIFT_FORMAT_VERSION > /dev/null; then
+      xcrun --sdk macosx mint bootstrap -m $MINT_FILE_PATH
+    fi
+
+    xcrun --sdk macosx mint run -m $MINT_FILE_PATH $SWIFT_FORMAT --config "$BASEDIR/.swiftformat" .
 fi

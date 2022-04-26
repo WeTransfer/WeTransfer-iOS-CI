487eca3c0b29419c08d074d09c4e0a3e3f216935
Raphael Silva <5387088+peagasilva@users.noreply.github.com>
2022-04-26 16:25:38 +0200

    Update `minimumLinesCount` default value to 240 in `WeTransferPRLinter.validateMarkUsage(using:file:lines:minimumLinesCount:)`

--- WeTransferPRLinter/Sources/WeTransferPRLinter/WeTransferPRLinter.swift
+++ WeTransferPRLinter/Sources/WeTransferPRLinter/WeTransferPRLinter.swift
@@ -174,7 +174,7 @@ extension WeTransferPRLinter {
     }
 
     /// Warns if a big files is containing any MARK comments.
-    static func validateMarkUsage(using danger: DangerDSL, file: Danger.File, lines: [String], minimumLinesCount: Int = 200) {
+    static func validateMarkUsage(using danger: DangerDSL, file: Danger.File, lines: [String], minimumLinesCount: Int = 240) {
         guard !file.lowercased().contains("test"), lines.count >= minimumLinesCount else { return }
         let containsMark = lines.contains(where: { line in line.contains("MARK:") })
         guard !containsMark else { return }

9d77817bb0e20c514e5579fd9d438dae3285d008
Raphael Silva <5387088+peagasilva@users.noreply.github.com>
2022-06-10 14:04:04 +0200

    Update formatting to only run on staged files and add the changes right away

--- BuildTools/swiftformat.sh
+++ BuildTools/swiftformat.sh
@@ -9,8 +9,11 @@ if [ -z "$CI" ]; then
     SWIFT_FORMAT_VERSION=$(grep -F $SWIFT_FORMAT $MINT_FILE_PATH)
 
     if ! xcrun --sdk macosx mint which -s $SWIFT_FORMAT_VERSION > /dev/null; then
-      xcrun --sdk macosx mint bootstrap -m $MINT_FILE_PATH
+        xcrun --sdk macosx mint bootstrap -m $MINT_FILE_PATH
     fi
 
-    xcrun --sdk macosx mint run -m $MINT_FILE_PATH $SWIFT_FORMAT --config "$BASEDIR/.swiftformat" .
+    git diff --diff-filter=d --staged --name-only | grep -e '\(.*\).swift$' | while read file; do
+        xcrun --sdk macosx mint run -m $MINT_FILE_PATH $SWIFT_FORMAT --config "$BASEDIR/.swiftformat" "${file}";
+        git add "$file";
+    done
 fi

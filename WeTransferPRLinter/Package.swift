63764d178e9233facc256075c2cb811cd7577e71
Antoine van der Lee <4329185+AvdLee@users.noreply.github.com>
2023-04-07 16:21:38 +0200

    Revert package update

--- WeTransferPRLinter/Package.swift
+++ WeTransferPRLinter/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "WeTransferPRLinter",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v10_15)
     ],
     products: [
         .library(
@@ -17,16 +17,16 @@ let package = Package(
     dependencies: [
         // Local package is required for development since Danger does not support Swift 5.5 correctly yet with Danger-Plugin testing.
 //         .package(name: "danger-swift", path: "../../../../../Forks/swift"),
-        .package(url: "https://github.com/danger/swift.git", from: "3.12.1"),
+               .package(name: "danger-swift", url: "https://github.com/danger/swift.git", from: "3.12.1"),
 //        .package(name: "danger-swift", url: "https://github.com/AvdLee/swift.git", .branch("master")),
-        .package(url: "https://github.com/JohnSundell/Files", from: "4.1.1"),
-        .package(url: "https://github.com/davidahouse/XCResultKit.git", from: "0.9.2")
+               .package(name: "Files", url: "https://github.com/JohnSundell/Files", from: "4.1.1"),
+               .package(name: "XCResultKit", url: "https://github.com/davidahouse/XCResultKit.git", from: "0.9.2")
     ],
     targets: [
         .target(
             name: "WeTransferPRLinter",
             dependencies: [
-                .product(name: "Danger", package: "swift"),
+                .product(name: "Danger", package: "danger-swift"),
                 "Files",
                 "XCResultKit"
             ]
@@ -35,7 +35,7 @@ let package = Package(
             name: "WeTransferPRLinterTests",
             dependencies: [
                 "WeTransferPRLinter",
-                .product(name: "DangerFixtures", package: "swift")
+                .product(name: "DangerFixtures", package: "danger-swift")
             ],
             resources: [
                 .copy("Resources/")

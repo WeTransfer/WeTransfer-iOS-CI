5e0549b2cfbd4dd8fee6722f1d1905cdb4ae7013
Leonardo Chiappalupi <lc@bendingspoons.com>
2025-01-09 15:34:40 +0100

    feat: make warnings reporting via Danger optional

--- WeTransferPRLinter/Sources/WeTransferPRLinter/XCResultReporting/XCResultSummaryReporter.swift
+++ WeTransferPRLinter/Sources/WeTransferPRLinter/XCResultReporting/XCResultSummaryReporter.swift
@@ -9,6 +9,7 @@ public protocol XCResultSummaryReporting {
     static func reportXCResultSummary(
         for files: [XCResultSummaryContaining],
         using danger: DangerDSL,
+        shouldReportWarnings: Bool,
         fileManager: FileManager,
         resultsFilter: ResultsFilter?
     )
@@ -19,6 +20,7 @@ public enum XCResultSummaryReporter: XCResultSummaryReporting {
     public static func reportXCResultSummary(
         for files: [XCResultSummaryContaining],
         using danger: DangerDSL,
+        shouldReportWarnings: Bool = false,
         fileManager: FileManager = .default,
         resultsFilter: ResultsFilter? = nil
     ) {
@@ -31,9 +33,18 @@ public enum XCResultSummaryReporter: XCResultSummaryReporting {
             return XCResultItemsFactory(resultFile: resultFile, resultsFilter: resultsFilter, fileManager: fileManager).make()
         }
 
-        results.forEach { result in
-            danger.report(result)
-        }
+        let resultsToReport = {
+            if !shouldReportWarnings {
+                let (filteredResults, warningsCount) = results.reduce(into: ([], 0)) { result, item in
+                    item.category == .warning ? result.1 += 1 : result.0.append(item)
+                }
+                danger.report(.init(message: "Project has \(warningsCount) warnings", category: .warning))
+                return filteredResults
+            } else {
+                return results
+            }
+        }()
+        resultsToReport.forEach(danger.report)
 
         XCResultCoverageReporter(resultFiles: resultFiles, danger: danger).report(minimumCoverage: 0.8)
     }
